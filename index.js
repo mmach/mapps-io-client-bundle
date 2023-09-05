63c0679407a4f904d071967640fbb8ca887df327
RXS\mmach-c <mmach-c@rxsinfo.com>
2023-09-05 13:18:21 +0200

    new build with proxy

--- index.js
+++ index.js
@@ -1,22 +1,69 @@
 const express = require('express');
 const path = require('path');
 var compression = require('compression')
+var dotenv = require('dotenv')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+dotenv.config()
+
+fs.readFile('./dist/index.html', (result, data) => {
+    const $ = cheerio.load(data.toString())
+    if ($('head').find('base').length > 0) {
+        $('base').remove()
+    }
+    if ($('head').find('base').length == 0) {
+        $('head').append(`<base href="${process.env.VITE_BASE_URL ? process.env.VITE_BASE_URL : ""}"/>`)
+    }
+
+    fs.writeFile('./dist/index.html', $.html(), (err) => { })
+
+})
+//const $ = cheerio.load('<h2 class="title">Hello world</h2>');
 
 const app = express();
 //const root = `${__dirname}/static`
+const options = {
+    target: process.env.VITE_API_URL, // target host
+    changeOrigin: true, // needed for virtual hosted sites
+    ws: false, // proxy websockets
+    pathRewrite: {
+        '^/command': '/command', // rewrite path
+        '^/query': '/query', // remove base path
+    },
 
+};
+const optionsBlob = {
+    target: process.env.VITE_BLOB_URL, // target host
+    changeOrigin: true, // needed for virtual hosted sites
+    ws: false, // proxy websockets
+    pathRewrite: {
+        '^/blob': '/blob', // rewrite path
+    },
+
+};
 app.use(compression());//add this as the 1st middleware
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'webcomponent')));
+
+const exampleProxy = createProxyMiddleware(options);
+const exampleBlobProxy = createProxyMiddleware(optionsBlob);
+
+// mount `exampleProxy` in web server
+app.use('/command', exampleProxy);
+app.use('/query', exampleProxy);
+app.use('/blob', exampleBlobProxy);
 
 // An api endpoint that returns a short list of 
 
 // Handles any requests that don't match the ones above
 //app.use(fallback('/www_build/index.html', { root }))
-
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 
